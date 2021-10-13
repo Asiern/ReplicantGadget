7803dc739100172ccb6190f6ce12506d4831f030
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2021-10-13 20:58:09 +0200

    csv to cpp

---
+++ Scripts/csv_to_cpp.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import getopt
+import csv
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hf:", ["file"])
+except getopt.GetoptError:
+    print('csv_to_cpp.py -f <file.csv>')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-h':
+        print('csv_to_cpp.py -f <file.csv>')
+        sys.exit()
+    elif opt in ("-f", "--file"):
+        path = arg
+    # elif opt in ("-p", "--prefix"):
+    #     prefix = arg
+    # elif opt in ("-s", "-sufix"):
+    #     sufix = arg
+
+
+f = open("out.cpp", "w")
+
+category = ""
+with open(path, newline="") as file:
+    reader = csv.reader(file, delimiter=",", quotechar="|")
+    next(reader)
+    for row in reader:
+        if  row[0] != "":
+            category = row[0]
+            f.write("\n// "+category+"\n")
+        f.write("_"+category+'->Add("' + row[1] + '");'+ "\n")
